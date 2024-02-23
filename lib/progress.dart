30b7ed94e3fcd01b64609297ebf4c53eb3c85a90
IU14 <iu14@canterbury.ac.uk>
2024-02-23 13:27:50 +0000

    Layout of progress page implemented. Created and uploaded a basic feedback file in JSON format

--- lib/progress.dart
+++ lib/progress.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mem_match_game/main.dart';
 
 class ProgressPage extends StatelessWidget {
   final ThemeData theme;
@@ -7,7 +9,45 @@ class ProgressPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Celebrate your progress!'),
+      ),
+      body: const Center(
+        // TO DO: Add the progress page information here
+      ),
+      bottomNavigationBar: Builder(
+        builder: (BuildContext context) {
+          return BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Back to Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.logout),
+                label: 'Exit',
+              ),
+            ],
+            currentIndex: 0,
+            onTap: (index) {
+              switch (index) {
+                case 0:
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(theme: theme),
+                    ),
+                  );
+                  break;
+                case 1:
+                  SystemNavigator.pop();
+                  break;
+              }
+            },
+          );
+        },
+      ),
+    );
   }
 }
